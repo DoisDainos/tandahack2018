2adbf1a319a953891eff3eeb9176abc0368f217c
Gianni Poutanen <g.poutanen@uqconnec.edu.au>
2018-04-21 14:06:51 +1000

    edit model

--- app/models/Model.rb
+++ app/models/Model.rb
@@ -2,7 +2,8 @@ include Math
 
 
 class User
-    attr_accessor(:level, :currentXp, :nextLevel, :attendanceStreak, :achievements)
+    attr_accessor(:level, :currentXp, :nextLevel, :attendanceStreak)
+    attr_reader(:achievements)
     def initialize(id, name, department)
         @level = 1
         @currentXp = 0
@@ -11,7 +12,14 @@ class User
         @id = id
         @name = name
         @department = department
-        @achievements = []
+        @achievements = instantiateAchievements
+    end
+
+    def instantiateAchievements
+        array = Array.new
+        array + Milestone("Weakly Streak", 10).generateMilestones(10, 5)
+        print array
+        return array
     end
 
     def giveXp(x)
@@ -21,7 +29,11 @@ class User
             @nextlevel = @nextLevel * (1+log2(level))
         end
     end
-    
+
+    def getValues(param)
+        return @currentXp
+    end
+
     def endStreak(x)
         @attendanceStreak = 0
     end
@@ -33,16 +45,44 @@ end
 
 class Achievement
     attr_accessor(:name, :xp)
+    attr_reader(:achieved)
     def initialize(name, xp)
         @xp = xp
         @name = name
+        @goal = 50
+        @achieved = false
     end
-    
+
+    def compareValues(user)
+        return user.getValues(@name).equal?(@goal)
+    end
+
     def giveAchievement(user)
         user.achievements.push(@name)
         user.giveXp(@xp)
+        @achieved = true
     end
 end
+
+class Milestone < Achievement
+    attr_accessor(:name, :xp)
+    attr_reader(:achieved)
+    def initialize
+        super(@name, @xp)
+    end
+
+    def generateMilestones(amount, factor)
+        array = Array.new
+        i = 0
+        while i < amount
+            array.push(Achievement(@name + " " + i, i * factor))
+            i += 1
+        end
+        return array
+    end
+end
+
+
 
 #Test Space
 
